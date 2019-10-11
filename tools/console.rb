91f07bc225d5ef03f30c1e62331530f0c125a0e6
Ryan <rngao97@gmail.com>
2019-10-11 11:04:49 -0400

    a

--- tools/console.rb
+++ tools/console.rb
@@ -1,10 +1,14 @@
 require_relative '../config/environment.rb'
+require_relative './driver.rb'
+require_relative './passenger.rb'
+require_relative './ride.rb'
+
 
 def reload
   load 'config/environment.rb'
 end
 
-
-# Put your variables here~!
+Passenger1 = Passenger.new("Person")
+p Passenger1.name
 
 binding.pry
